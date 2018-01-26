aa8c9d83563bc5ae89531725eee80e176ac83a29
duanlei <455483293@qq.com>
2018-01-26 17:39:34 +0800

    Initial commit

---
+++ app/src/main/java/com/ap88/yg/fruittole/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ap88.yg.fruittole
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun test(view: View) {
+        Person("hello").niceToast(this, "message")
+    }
+
+}
